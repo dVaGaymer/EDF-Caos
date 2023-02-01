c747ea4ad8f30aa3a4b9cf92c6fe0a4a635601cd
Alvaro Lopez <al7aro@gmail.com>
2023-02-02 00:09:34 +0100

    m13rd4_d3_c0d1g0

--- ej1_2_3.jl
+++ ej1_2_3.jl
@@ -4,13 +4,14 @@ using GLMakie
 running = Observable(false)
 
 # N of iterations
-it = 100
+it = 200
 
 # Initial conditions
 x_init = 0.1
+μ_init = 3.566
 Base.@kwdef mutable struct Logistic
-	n = 0
-	μ = 3.2
+	n = 1
+	μ = μ_init
 	x = x_init
 	f::Function = (μ_aux, x_aux) -> μ_aux * x_aux * (1 - x_aux)
 end
@@ -32,16 +33,117 @@ function step!(l::Logistic)
 	Point2f(x, y)
 end
 
+# functions
+function anim_next()
+	if (running[] == true)
+		return
+	end
+	push!(data[], step!(rep))
+	data[] = data[]
+end
+function anim_prev()
+	if (running[] == true || length(data[]) == 1)
+		return
+	end
+	pop!(data[])
+	rep.x = last(data[])[1]
+	rep.n -= 1
+	data[] = data[]
+end
+function anim_reset(x, μ)
+	splice!(data[], 1:length(data[]))
+	rep.μ = μ
+	rep.x = x
+	rep.n = 1
+	push!(data[], [x, 0.0])
+	data[] = data[]
+end
+function anim_init()
+	@async while (running[] == true)
+		while (rep.n < it && running[] == true)
+			push!(data[], step!(rep))
+			sleep(0.1)
+			data[] = data[]
+		end
+		while (rep.n > 1 && running[] == true)
+			pop!(data[])
+			rep.x = last(data[])[1]
+			rep.n -= 1
+			sleep(0.1)
+			data[] = data[]
+		end
+	end
+end
+
 # Initalize logistic ecuation
 rep = Logistic()
 data = Observable(Point2f[])
-push!(data[], step!(rep))
+anim_reset(x_init, μ_init)
+func = Observable{Function}(x -> rep.f(rep.μ, x))
 
 #= PLOTTING =#
 fig = Figure()
 axis = Axis(fig[1, 1:3])
 lines!(axis, data)
-lines!(axis, 0:0.01:1, x -> rep.f(rep.μ, x))
+lines!(axis, 0:0.01:1, func)
 lines!(axis, 0:0.01:1, x -> x)
 
-include("events.jl")
+# Initialize sliders
+μSlider = Slider(fig[1, 4];
+		horizontal = false,
+		range = 0.0:0.1:4,
+		startvalue = 3.566,
+		color_inactive = RGBf(0.90, 0.50, 0.50),
+		color_active = RGBf(0.70, 0.20, 0.20),
+		color_active_dimmed = RGBf(0.70, 0.20, 0.20))
+xSlider = Slider(fig[2, 1:3];
+		horizontal = true,
+		range = 0.0:0.01:1,
+		startvalue = 0.1,
+		color_inactive = RGBf(0.50, 0.50, 0.90),
+		color_active = RGBf(0.20, 0.20, 0.70),
+		color_active_dimmed = RGBf(0.20, 0.20, 0.70))
+
+lift(xSlider.value) do v
+	running[] = false
+	n = rep.n
+	anim_reset(v, rep.μ)
+	data_aux = Point2f[]
+	push!(data_aux, [v, 0.0])
+	while (n > 1)
+		push!(data_aux, step!(rep))
+		n -= 1
+	end
+	data[] = data_aux
+end
+
+lift(μSlider.value) do v
+	running[] = false
+	func[] = (x -> rep.f(v, x))
+	n = rep.n
+	anim_reset(rep.x, v)
+	data_aux = Point2f[]
+	push!(data_aux, [rep.x, 0.0])
+	while (n > 1)
+		push!(data_aux, step!(rep))
+		n -= 1
+	end
+	data[] = data_aux
+end
+
+#Intialice button and its functions
+initAnimation = Button(fig[1, 2, Top()]; label = "Start/Stop Animation")
+stepOut = Button(fig[3, 1]; label = "Step Out")
+resetAnim = Button(fig[3, 2]; label = "Reset")
+stepIn = Button(fig[3, 3]; label = "Step In")
+stepIn.tellwidth = false
+stepOut.tellwidth = false
+resetAnim.tellwidth = false
+
+on(stepIn.clicks) do clicks; anim_next(); end
+on(stepOut.clicks) do clicks; anim_prev(); end
+on(resetAnim.clicks) do clicks; anim_reset(rep.x, rep.μ); end
+on(initAnimation.clicks) do clicks; anim_init(); end
+on(initAnimation.clicks) do clicks; running[] = !running[]; end
+
+display(fig)
