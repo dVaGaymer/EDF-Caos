82109ab0c5c825b31727e666f72f10f473cc7a48
Alvaro Lopez <al7aro@gmail.com>
2023-02-09 13:13:15 +0100

    lyapunov camp

--- Sensitivity/campLyapunov.jl
+++ Sensitivity/campLyapunov.jl
@@ -3,30 +3,8 @@ using GLMakie
 include("lyapunov.jl")
 
 #= Logistic equation and its data =#
-function campFunc(μ, x)
-	if (x < 0 || x > 1)
-		return 0
-	end
-	if (0 < x < 1/2)
-		2 * μ * x
-	else
-		if (1/2 < x < 1)
-			2 * μ - 2 * μ * x
-		end
-	end
-end
-function campFunc_der(μ, x)
-	if (x < 0 || x > 1)
-		return 0
-	end
-	if (0 < x < 1/2)
-		2 * μ
-	else
-		if (1/2 < x < 1)
-			- 2 * μ
-		end
-	end
-end
+campFunc(μ, x) = (0 < x < 1/2) ? 2 * μ * x : (1/2 < x < 1) ? 2 * μ - 2 * μ * x : 0
+campFunc_der(μ, x) = (0 < x < 1/2) ? 2 * μ : (1/2 < x < 1) ? - 2 * μ : 0
 campEc = DynamicSyistem(;f = (μ, x) -> campFunc(μ, x))
 campEc = DynamicSyistem(;f_der = (μ, x) -> campFunc_der(μ, x))
 μ_data = 3.4:0.0001:4
@@ -37,4 +15,4 @@ fig = Figure()
 ax = Axis(fig[1, 1]; xlabel = "μ", xlabelsize = 50, ylabel = "λ", ylabelsize = 50)
 # scatter!(μ_data, λ_data; markersize = 3)
 scatter(μ_data, λ_data)
-
+lines!(0..10, x -> campFunc(2.5, x))
