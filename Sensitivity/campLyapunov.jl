c9bccba59909ba23a630aad430f8f2a094276a97
Alvaro Lopez <al7aro@gmail.com>
2023-02-09 12:55:37 +0100

    camp test

--- Sensitivity/campLyapunov.jl
+++ Sensitivity/campLyapunov.jl
@@ -3,8 +3,32 @@ using GLMakie
 include("lyapunov.jl")
 
 #= Logistic equation and its data =#
-campEc = DynamicSyistem(;f = (μ, x) -> 2 * μ * x)
-campEc = DynamicSyistem(;f_der = (μ, x) -> 2 * μ)
+function campFunc(μ, x)
+	if (x < 0 || x > 1)
+		return 0
+	end
+	if (0 < x < 1/2)
+		2 * μ * x
+	else
+		if (1/2 < x < 1)
+			2 * μ - 2 * μ * x
+		end
+	end
+end
+function campFunc_der(μ, x)
+	if (x < 0 || x > 1)
+		return 0
+	end
+	if (0 < x < 1/2)
+		2 * μ
+	else
+		if (1/2 < x < 1)
+			- 2 * μ
+		end
+	end
+end
+campEc = DynamicSyistem(;f = (μ, x) -> campFunc(μ, x))
+campEc = DynamicSyistem(;f_der = (μ, x) -> campFunc_der(μ, x))
 μ_data = 3.4:0.0001:4
 λ_data = lyapunov.([campEc], μ_data, 1000)
 
@@ -12,5 +36,5 @@ campEc = DynamicSyistem(;f_der = (μ, x) -> 2 * μ)
 fig = Figure()
 ax = Axis(fig[1, 1]; xlabel = "μ", xlabelsize = 50, ylabel = "λ", ylabelsize = 50)
 # scatter!(μ_data, λ_data; markersize = 3)
-lines!(μ_data, λ_data)
+scatter(μ_data, λ_data)
 
--- UnidimensionalDiscreteDynamicSystem/feigenbaum_create.jl
+++ UnidimensionalDiscreteDynamicSystem/feigenbaum_create.jl
@@ -13,7 +13,7 @@ function getAtractors(μ)
 	push!(aux_data, x_init[])
 	for i = 1:1000
 		new_x = ec(μ, aux_data[i])
-		fa = findall(x -> round(x; digits = dec_digits) == round(new_x; digits = dec_digits), aux_data)
+		fa = findall(x -> round(x; digits = dec_digits) == round(x; digits = dec_digits), aux_data)
 		if (length(fa) != 0)
 			push!(aux_data, new_x)
 			return addPoints(fa[1], i + 1, aux_data, μ)
@@ -22,8 +22,17 @@ function getAtractors(μ)
 	end
 	1000
 end
-x_data = fill(0, μ_steps)
+# μ_steps = 100
+# x_data = fill(0, μ_steps)
+# x_init = Observable(0.1)
+# ec = campFunc
+# dec_digits = 5
+# μ_data = range(2.8, 4, μ_steps)
+# data_path = "camp.csv"
 for i = 1:μ_steps
 	getAtractors(μ_data[i])
 end
-CSV.write(data_path, aux_P)
+# CSV.write(data_path, aux_P)
+# P[] = (Matrix(CSV.read(data_path, DataFrame; delim = ',', header = false)))::Matrix
+# scatter(P; markersize = 5)
+
